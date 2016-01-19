7595471c994bdde1d961f3da3f93c83413bedb48
Matt <tam.nguyen@ffwagency.com>
2016-01-19 15:25:09 +0700

    Update table group member

--- src/db/group_member.sql
+++ src/db/group_member.sql
@@ -2,8 +2,8 @@ DROP TABLE `group_member`;
 
 CREATE TABLE IF NOT EXISTS `group_member` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `gid` int(11) NOT NULL,
-  `mid` int(11) NOT NULL,
+  `groupid` int(11) NOT NULL,
+  `memberid` int(11) NOT NULL,
   `isdeleted` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
