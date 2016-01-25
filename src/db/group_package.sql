7e3fb6c012163100eedc86e91c45c60f39486852
Matt <tam.nguyen@ffwagency.com>
2016-01-25 08:31:16 +0700

    Add column efftoextend to group_package table

--- src/db/group_package.sql
+++ src/db/group_package.sql
@@ -27,3 +27,4 @@ ALTER TABLE `group_package`
 ALTER TABLE `group_package`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `group_package`  ADD `visitorprice` INT NULL DEFAULT NULL ;
+ALTER TABLE `group_package`  ADD `efftoextend` INT NULL DEFAULT NULL ;
