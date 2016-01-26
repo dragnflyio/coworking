9a9c4ca6f7ff9aee84cb0727e0ef06e17ce6d4e8
cuongnguyenvn <webtomme@gmail.com>
2016-01-26 10:53:29 +0700

    card reader information

---
+++ src/db/card_infomation.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `card_infomation` (
+  `id` int(11) NOT NULL,
+  `cardid` varchar(1000) DEFAULT NULL,
+  `cardcontent` varchar(255) DEFAULT NULL,
+  `createdtime` int(11) NOT NULL,
+  `sysdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+ALTER TABLE `card_infomation`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `card_infomation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
