cb501ec57c303a905134ad7862d9986a843c7072
Matt <tam.nguyen@ffwagency.com>
2016-02-01 17:36:11 +0700

    Update room schedule

--- src/db/room_schedule.sql
+++ src/db/room_schedule.sql
@@ -12,11 +12,3 @@ CREATE TABLE IF NOT EXISTS `room_schedule` (
   `note` varchar(1000) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `room_schedule`
---
-
-INSERT INTO `room_schedule` (`id`, `roomid`, `state`, `fromtime`, `totime`, `relatedusers`, `createdby`, `createdtime`, `sysdate`, `note`) VALUES
-(1, 2, 1, 1454315700, 1454319300, 7, 0, 0, '2016-02-01 08:40:08', 'Đặt phòng họp 1'),
-(2, 3, 1, 1454316900, 1454320500, 8, 0, 0, '2016-02-01 08:57:26', 'Drupal');
